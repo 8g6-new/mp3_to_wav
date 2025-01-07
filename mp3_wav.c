6b2ab8367ff87c4a640b2d0914fff26642d3ff97
8g6 <127236588+8g6-new@users.noreply.github.com>
2025-01-08 04:52:29 +0530

    first commit

---
+++ mp3_wav.c
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <sys/stat.h>
+#define MINIMP3_ONLY_MP3
+#define MINIMP3_USE_SIMD
+#define MINIMP3_IMPLEMENTATION
+#define MINIMP3_FLOAT_OUTPUT
+
+#ifdef MINIMP3_FLOAT_OUTPUT
+    typedef float DATA;
+    #define writeWave write_float_wav
+#else
+    typedef short DATA;
+    #define writeWave write_pcm_wav
+#endif
+
+#include "minimp3.h"
+
+#include "wav.c"
+
+#define MAX_SLICES 100
+#define DELIMITER ","
+
+typedef struct  {
+    int sr;
+    int bit_rate;
+    int channels;
+} info;
+
+
+
+void readFile(const char *filename, uint64_t *size, uint8_t **data) {
+    *size = 0;
+    *data = NULL;
+
+    FILE *fin = fopen(filename, "rb");
+    if (!fin) {
+        printf("\nError opening input file\n");
+        perror("fopen");
+        return;
+    }
+
+    struct stat st;
+    if (fstat(fileno(fin), &st) != 0) {
+        perror("fstat");
+        fclose(fin);
+        return;
+    }
+
+    *size = (uint64_t)st.st_size;
+
+    *data = malloc(*size);
+    if (!*data) {
+        fprintf(stderr, "Memory allocation failed\n");
+        *size = 0;
+        fclose(fin);
+        return;
+    }
+
+    if (fread(*data, 1, *size, fin) != *size) {
+        perror("fread");
+        free(*data);
+        *data = NULL;
+        *size = 0;
+    }
+
+    fclose(fin);
+}
+
+
+info slice_mp3_to_wav(char *input_filename, char *output_fn, float lengths[][2], unsigned short length) {
+    info INFO               = {0};
+
+    static mp3dec_t mp3d;
+    mp3dec_init(&mp3d);
+
+    uint64_t buf_size       = 0;
+    uint8_t *input_buf      = NULL;
+    uint8_t *orig_input_buf = NULL;  
+
+    readFile(input_filename, &buf_size, &input_buf);
+    if (!input_buf) {
+        fprintf(stderr, "Failed to read input file: %s\n", input_filename);
+        return INFO;
+    }
+
+    orig_input_buf         = input_buf;  
+
+    size_t data_size       = sizeof(DATA); 
+
+    size_t max_pcm_samples = (buf_size * MINIMP3_MAX_SAMPLES_PER_FRAME)/ 128; 
+    size_t pcm_bsiz        = max_pcm_samples * data_size * 2;
+    DATA *full_pcm         = malloc(pcm_bsiz);
+    
+    if (!full_pcm) {
+        fprintf(stderr, "Memory allocation failed for full_pcm\n");
+        free(orig_input_buf);
+        return INFO;
+    }
+
+    uint64_t decoded_samples = 0;
+    size_t remaining_size = buf_size;
+
+    while (remaining_size > 0) {
+        mp3dec_frame_info_t info;
+        DATA pcm[MINIMP3_MAX_SAMPLES_PER_FRAME * 2]; 
+        int samples = mp3dec_decode_frame(&mp3d, input_buf, remaining_size, pcm, &info);
+
+        if (info.frame_bytes == 0 || remaining_size < info.frame_bytes) {
+            break; 
+        }
+
+        if (samples > 0) {
+            if ((decoded_samples + samples) * 2 > max_pcm_samples) {
+                fprintf(stderr, "PCM buffer overflow prevented\n");
+                break;
+            }
+
+            size_t copy_size = samples * data_size * info.channels;
+            memcpy(full_pcm + (decoded_samples * info.channels), pcm, copy_size);
+            
+            INFO.channels   = info.channels;
+            INFO.sr         = info.hz;
+            decoded_samples += samples;
+        }
+
+        input_buf           += info.frame_bytes;
+        remaining_size      -= info.frame_bytes;
+    }
+    // if (output_fn) {
+    //     char full_output[512];
+    //     snprintf(full_output, sizeof(full_output), "%s_full.wav", output_fn);
+    //     writeWave(full_output, full_pcm, decoded_samples, INFO.channels, INFO.sr, INFO.bit_rate);
+    // }
+
+    for (int i = 0; i < length; i++) {
+        if (lengths[i][0] < 0 || lengths[i][1] <= lengths[i][0] || 
+            lengths[i][1] > (float)decoded_samples / INFO.sr) {
+            fprintf(stderr, "Invalid time range for slice %d: [%f, %f]\n", i + 1, lengths[i][0], lengths[i][1]);
+            continue;
+        }
+
+        uint64_t start_sample = (uint64_t)(lengths[i][0] * INFO.sr) * INFO.channels;
+        uint64_t end_sample = (uint64_t)(lengths[i][1] * INFO.sr) * INFO.channels;
+        uint64_t slice_samples = end_sample - start_sample;
+
+        DATA *slice = malloc(slice_samples * data_size);
+        if (!slice) {
+            fprintf(stderr, "Memory allocation failed for slice %d\n", i + 1);
+            continue;
+        }
+
+        memcpy(slice, full_pcm + start_sample, slice_samples * data_size);
+
+        char output_filename[512];
+        snprintf(output_filename, sizeof(output_filename), "%s_slice_%d.wav", output_fn, i + 1);
+        
+        writeWave(output_filename, slice, slice_samples/ INFO.channels,INFO.channels, INFO.sr, INFO.bit_rate);
+
+        free(slice);
+    }
+
+    free(full_pcm);
+    free(orig_input_buf);
+    return INFO;
+}
+
+
+unsigned short get_lengths(char *starts, char *ends, float lengths[][2]) {
+    unsigned short count = 0;
+
+    char *rest_starts = starts;
+    char *rest_ends   = ends;
+
+    char *start_token = strtok_r(rest_starts, DELIMITER, &rest_starts);
+    char *end_token   = strtok_r(rest_ends, DELIMITER, &rest_ends);
+
+    while (start_token != NULL && end_token != NULL && count < MAX_SLICES) {
+
+        float start_value = atof(start_token);
+        float end_value   = atof(end_token);
+
+        lengths[count][0]  = start_value;
+        lengths[count][1]  = end_value;
+
+        count++;
+
+        start_token = strtok_r(NULL, DELIMITER, &rest_starts);
+        end_token   = strtok_r(NULL, DELIMITER, &rest_ends);
+    }
+
+    return count;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s <input mp3> <output fn> <starts> <ends>\n", argv[0]);
+        return 1;
+    }
+    
+    float lengths[MAX_SLICES][2];
+
+    char *input_filename  = argv[1];
+    char *output_filename = argv[2];
+    char *starts          = strdup(argv[3]);
+    char *ends            = strdup(argv[4]);
+
+    if (!starts || !ends) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+
+    unsigned short length = get_lengths(starts, ends, lengths);
+
+    slice_mp3_to_wav(input_filename,output_filename,lengths, length);
+}
+
+
+//gcc -mavx -o mp3_to_wav mp3_wav.c -O3 -march=native -ffast-math -funroll-loops -lm
+//./slice_mp3_to_wav "../tests/files/25/Black Woodpecker/0.mp3" "./a.wav" "1,2" "2,3"
+
---
+++ wav.c
@@ -0,0 +1,105 @@
+#include <stdint.h>
+
+#define WAV_FORMAT_PCM   0x0001
+#define WAV_FORMAT_FLOAT 0x0003
+
+#pragma pack(push, 1)
+typedef struct {
+    char     riff[4];         /* "RIFF"                                  */
+    uint32_t file_length;     /* file length in bytes                    */
+    char     wave[4];         /* "WAVE"                                  */
+    char     fmt[4];          /* "fmt "                                  */
+    uint32_t chunk_size;      /* size of FMT chunk in bytes (usually 16) */
+    uint16_t format_tag;      /* 1=PCM, 3=IEEE float                    */
+    uint16_t num_channels;    /* 1=mono, 2=stereo                       */
+    uint32_t sample_rate;     /* Sampling rate in samples per second     */
+    uint32_t bytes_per_sec;   /* bytes per second                       */
+    uint16_t block_align;     /* bytes per sample * num_channels        */
+    uint16_t bits_per_sample; /* Number of bits per sample              */
+    char     data[4];         /* "data"                                 */
+    uint32_t data_length;     /* data length in bytes                   */
+} wav_header;
+#pragma pack(pop)
+
+
+static void init_wav_header(wav_header* header, int format_tag, int channels, int sample_rate, int bits_per_sample, uint32_t data_length) {
+    memcpy(header->riff, "RIFF", 4);
+    memcpy(header->wave, "WAVE", 4);
+    memcpy(header->fmt,  "fmt ", 4);
+    memcpy(header->data, "data", 4);
+
+    header->chunk_size      = 16; 
+    header->format_tag      = format_tag;
+    header->num_channels    = channels;
+    header->sample_rate     = sample_rate;
+    header->bits_per_sample = bits_per_sample;
+    header->block_align     = channels * (bits_per_sample / 8);
+    header->bytes_per_sec   = sample_rate * header->block_align;
+    header->data_length     = data_length;
+    header->file_length     = data_length + sizeof(wav_header) - 8;  
+}
+
+int write_pcm_wav(const char *filename, const int16_t *pcm, uint32_t sample_count, int channels, int sample_rate, int kbits_per_sec) {
+    
+    wav_header header;
+    uint32_t data_length = sample_count * channels * sizeof(int16_t);
+    
+    init_wav_header(&header, WAV_FORMAT_PCM, channels, sample_rate, 16, data_length);
+
+    FILE *fout = fopen(filename, "wb");
+    if (!fout) {
+        perror("Error opening file for writing");
+        return -1;
+    }
+
+    if (fwrite(&header, sizeof(header), 1, fout) != 1) {
+        perror("Error writing WAV header");
+        fclose(fout);
+        return -1;
+    }
+
+    if (fwrite(pcm, 1, data_length, fout) != data_length) {
+        perror("Error writing WAV data");
+        fclose(fout);
+        return -1;
+    }
+    else{
+        printf("%s PCM 16bit WAV file written successfully.\n",filename);
+    }
+
+
+    fclose(fout);
+    return 0;
+}
+
+int write_float_wav(const char *filename, const float *pcm, uint32_t sample_count,int channels, int sample_rate, int kbits_per_sec) {
+    
+    wav_header header;
+    uint32_t data_length = sample_count * channels * sizeof(float);
+
+    init_wav_header(&header,WAV_FORMAT_FLOAT, channels, sample_rate, 32, data_length);
+    
+    FILE *fout = fopen(filename, "wb");
+    if (!fout) {
+        perror("Error opening file for writing");
+        return -1;
+    }
+
+    if (fwrite(&header, sizeof(header), 1, fout) != 1) {
+        perror("Error writing WAV header");
+        fclose(fout);
+        return -1;
+    }
+
+    if (fwrite(pcm, 1, data_length, fout) != data_length) {
+        perror("Error writing WAV data");
+        fclose(fout);
+        return -1;
+    }
+    else{
+        printf("%s Float 32 bit WAV file written successfully.\n",filename);
+    }
+
+    fclose(fout);
+    return 0;
+}
